33afc7028e85655adc4eb4be7abf69cf298aace3
Alison Bonham <alison@alisonmae.com>
2016-03-08 12:23:18 -0700

    refining cello

--- cello_app/app/controllers/todos_controller.rb
+++ cello_app/app/controllers/todos_controller.rb
@@ -1,2 +1,16 @@
 class TodosController < ApplicationController
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+  @todo = Todo.new
+  @todo.item = params["todo"]["item"]
+  @todo.list_id = params["todo"]["list_id"]
+  @todo.save
+    #
+    redirect_to '/lists'
+
+  end
 end
--- cello_app/config/routes.rb
+++ cello_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   resources :todos
   resources :lists
+
+
+root "lists#index"
 end
